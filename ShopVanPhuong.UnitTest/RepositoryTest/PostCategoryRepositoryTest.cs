707188f2bd45ea1de96998a30f08934ca029d98a
ADMIN\Admin <phuongpro127@gmail.com>
2018-12-05 23:10:58 +0700

    add web api

---
+++ ShopVanPhuong.UnitTest/RepositoryTest/PostCategoryRepositoryTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShopVanPhuong.Data.Infrastructure;
+using ShopVanPhuong.Data.Repositories;
+using ShopVanPhuong.Model.Models;
+
+namespace ShopVanPhuong.UnitTest.RepositoryTest
+{
+    [TestClass]
+    public class PostCategoryRepositoryTest
+    {
+        IDbFactory dbFactory;
+        IPostCategoryRepository objRepository;
+        IUnitOfWork unitOfWork;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            dbFactory = new DbFactory();
+            objRepository = new PostCategoryRepository(dbFactory);
+            unitOfWork = new UnitOfWork(dbFactory);
+        }
+
+        [TestMethod]
+        public void PostCategory_Repository_Create()
+        {
+            PostCategory category = new PostCategory();
+            category.Name = "Test category";
+            category.Alias = "Test-category";
+            category.Status = true;
+
+            var result = objRepository.Add(category);
+            unitOfWork.Commit();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.ID);
+        }
+
+    }
+}
