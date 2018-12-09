c572606f9b5daf0771075816001375dfcb5d2986
ADMIN\Admin <phuongpro127@gmail.com>
2018-12-09 10:32:58 +0700

    Autofac để triển khai Dependency Injection

---
+++ ShopVanPhuong.Web/App_Start/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using Microsoft.Owin;
+using Owin;
+using ShopVanPhuong.Data;
+using ShopVanPhuong.Data.Infrastructure;
+using ShopVanPhuong.Data.Repositories;
+using ShopVanPhuong.Service;
+
+[assembly: OwinStartup(typeof(ShopVanPhuong.Web.App_Start.Startup))]
+
+namespace ShopVanPhuong.Web.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+
+        private void ConfigAutofac(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly()); //Register WebApi Controllers
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+
+            //InstancePerRequest là mỗi séc sừn nó sẽ tạo 1 đối tượng AsSelf()
+            builder.RegisterType<ShopVanPhuongDbContext>().AsSelf().InstancePerRequest();
+
+            // Repositories 1
+            builder.RegisterAssemblyTypes(typeof(PostCategoryRepository).Assembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerRequest();
+
+            // Services 2
+            builder.RegisterAssemblyTypes(typeof(PostCategoryService).Assembly)
+               .Where(t => t.Name.EndsWith("Service"))
+               .AsImplementedInterfaces().InstancePerRequest();
+
+            // gán tất cả cái 1,2 vào 1 cái thùng chứa
+            Autofac.IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            //Set the WebApi DependencyResolver
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver((IContainer)container); 
+
+        }
+    }
+}
