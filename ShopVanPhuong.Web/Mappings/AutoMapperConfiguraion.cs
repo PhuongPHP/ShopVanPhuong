cda5ed61e364fa7065aa96ce0ea883b9ccbcf792
ADMIN\Admin <phuongpro127@gmail.com>
2018-12-09 14:36:10 +0700

    Automapper for web

---
+++ ShopVanPhuong.Web/Mappings/AutoMapperConfiguraion.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ShopVanPhuong.Model.Models;
+using ShopVanPhuong.Web.Models;
+
+
+namespace ShopVanPhuong.Web.Mappings
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Post, PostViewModel>();
+                cfg.CreateMap<PostCategory, PostCategoryViewModel>();
+                cfg.CreateMap<Tag, TagViewModel>();
+            });
+        }
+    }
+}
